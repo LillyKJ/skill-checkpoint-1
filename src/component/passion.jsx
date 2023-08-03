59c8633b197e14333b3a1f143b1157e915bec443
Ly <korakot.jarunongkran>
2023-08-03 21:30:48 +0700

    feat: add text in passion.jsx

--- src/component/passion.jsx
+++ src/component/passion.jsx
@@ -6,9 +6,18 @@ function Passion() {
       <div className="Container">
         <h1 className="Topic">Additional passions</h1>
         <div className="Container-box">
-          <div className="Box"></div>
-          <div className="Box"></div>
-          <div className="Box"></div>
+          <div className="Box">
+            <h3>Front-End Developer</h3>
+            <h4>(HTML5, CSS, JS, React)</h4>
+          </div>
+          <div className="Box">
+            <h3>Front-End Developer</h3>
+            <h4>(HTML5, CSS, JS, React)</h4>
+          </div>
+          <div className="Box">
+            <h3>Front-End Developer</h3>
+            <h4>(HTML5, CSS, JS, React)</h4>
+          </div>
         </div>
       </div>
     </div>
