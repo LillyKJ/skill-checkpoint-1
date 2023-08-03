b11f142ffdfb2fbe881a9d9f34c3bad691c44e46
Ly <korakot.jarunongkran>
2023-08-03 19:50:10 +0700

    feat: can click on icon to navigate to web and animation on icon in contact.jsx and css

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -4,6 +4,21 @@ import Ig from "../img/Ig.png";
 import Email from "../img/Email.png";
 import Telp from "../img/Telp.png";
 
+const Link = (props) => {
+  return (
+    <a href={props.url} target="_blank">
+      {props.children}
+    </a>
+  );
+};
+
+const handleEmailClick = () => {
+  window.prompt("Email: ", "korakot.jarunongkran@gmail.com");
+};
+const handleTelpClick = () => {
+  window.prompt("Telp: ", "+66 639975999");
+};
+
 function Contact() {
   return (
     <div className="Contact">
@@ -11,19 +26,23 @@ function Contact() {
         <h1>Contact Me</h1>
         <div className="Choices-c">
           <div className="Box-c">
-            <img src={Git} alt="Git" />
+            <Link url="https://github.com/LillyKJ">
+              <img src={Git} alt="Git" />
+            </Link>
             <h1>Github</h1>
           </div>
           <div className="Box-c">
-            <img src={Ig} alt="Git" />
+            <Link url="https://www.instagram.com/likoralkolyt/">
+              <img src={Ig} alt="Git" />
+            </Link>
             <h1>Instagram</h1>
           </div>
           <div className="Box-c">
-            <img src={Email} alt="Git" />
+              <img src={Email} alt="Git" onClick={handleEmailClick} />
             <h1>Email</h1>
           </div>
           <div className="Box-c">
-            <img src={Telp} alt="Git" />
+            <img src={Telp} alt="Git" onClick={handleTelpClick}/>
             <h1>Telp</h1>
           </div>
         </div>
